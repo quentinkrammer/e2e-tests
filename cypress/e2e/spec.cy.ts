ac1e440da4975418045071762e71cefe03552cdc
bastian Opatz <bastian.opatz@brockhaus-ag.de>
2024-10-30 10:54:20 +0100

    vanilla cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,5 +1,8 @@
-describe("template spec", () => {
-  it("passes", () => {
+describe("cypress vanilla", () => {
+  it("types into input", () => {
     cy.visit("/");
+    cy.get("h1").should("contain.text", "Hello :)");
+    cy.wait(3000);
+    cy.get(".e2e-input").type("Chuck Norris");
   });
 });
