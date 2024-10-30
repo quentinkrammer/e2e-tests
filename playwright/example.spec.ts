b3e8a6a0dc4c1fe19ae7ab80a8b8e51774569880
bastian Opatz <bastian.opatz@brockhaus-ag.de>
2024-10-30 14:31:28 +0100

    flaky playwright test example

--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -1,12 +1,22 @@
 import { expect, test } from "@playwright/test";
 
 test("types into input", async ({ page }) => {
-  await page.goto("/");
+  page.goto("/");
 
   const h1 = page.getByRole("heading");
-  await expect(h1).toHaveText("Hello :)");
+  expect(h1).toHaveText("Hello :)");
 
   const input = page.locator(".e2e-input");
+
   input.fill("Chuck Norris");
-  expect(input).toHaveValue("Chuck Norris");
+  input.fill("Tom Scott");
+  input.fill("Ash Catchem");
+  input.fill("James Bond");
+  input.fill("Indiana Jones");
+  input.fill("Luke Skywalker");
+  input.fill("James O'Brian");
+
+  await expect(input).toHaveValue("Chuck NorrisTom Scott");
+  // there is more:
+  // https://jadala-ajay16.medium.com/why-do-we-write-await-async-in-playwright-javascript-typescript-fa3c92f82841
 });
