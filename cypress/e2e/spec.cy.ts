73d6d7335ba91f105c2840830e85210b6fc0afcf
bastian Opatz <bastian.opatz@brockhaus-ag.de>
2024-11-04 13:00:15 +0100

    remove wait statement from cypress test

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -2,7 +2,6 @@ describe("cypress vanilla", () => {
   it("types into input", () => {
     cy.visit("/");
     cy.get("h1").should("contain.text", "Hello :)");
-    cy.wait(3000);
 
     cy.get(".e2e-input")
       .type("Chuck Norris")
