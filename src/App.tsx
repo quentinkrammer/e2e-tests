54337adaa4601fcd64846cf1f7d926ab6bac1655
bastian Opatz <bastian.opatz@brockhaus-ag.de>
2024-11-07 08:27:22 +0100

    delayed disable

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,37 @@
+import { useEffect, useState } from "react";
 import "./App.css";
 
 function App() {
+  const disabled = useDelayedDisable();
+
   return (
     <>
       <div
         style={{
           width: "100vw",
-          height: "100vh",
           display: "grid",
           placeItems: "center",
         }}
       >
         <h1>Hello :)</h1>
-      </div>
-      <div>
-        <input type="text" className="e2e-input"></input>
+        <input disabled={disabled} type="text" className="e2e-input"></input>
       </div>
     </>
   );
+}
+
+function useDelayedDisable() {
+  const [disabled, setDisabled] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setDisabled(false);
+    }, 2000);
+
+    return () => clearTimeout(timer);
+  }, []);
+
+  return disabled;
 }
 
 export default App;
