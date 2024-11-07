79fea9a027fd8e15f8efec323ae0e0e87a5563a1
bastian Opatz <bastian.opatz@brockhaus-ag.de>
2024-11-07 09:55:56 +0100

    comments

--- cypress/e2e/spec.cy.ts
+++ cypress/e2e/spec.cy.ts
@@ -1,8 +1,11 @@
 describe("cypress vanilla", () => {
   it("types into input", () => {
     cy.visit("/");
+
+    // select + assert
     cy.get("h1").should("contain.text", "Hello :)");
 
+    // select + act + assert
     cy.get(".e2e-input")
       .type("Chuck Norris")
       .should("have.value", "Chuck Norris");
--- playwright/example.spec.ts
+++ playwright/example.spec.ts
@@ -3,11 +3,14 @@ import { expect, test } from "@playwright/test";
 test("types into input", async ({ page }) => {
   page.goto("/");
 
+  // select
   const h1 = page.getByRole("heading");
+  // assert
   expect(h1).toHaveText("Hello :)");
 
+  // select
   const input = page.locator(".e2e-input");
-
+  // act
   input.fill("Chuck Norris");
   input.fill("Tom Scott");
   input.fill("Ash Catchem");
@@ -15,8 +18,9 @@ test("types into input", async ({ page }) => {
   input.fill("Indiana Jones");
   input.fill("Luke Skywalker");
   input.fill("James O'Brian");
-
+  //assert
   await expect(input).toHaveValue("Chuck NorrisTom Scott");
+
   // there is more:
   // https://jadala-ajay16.medium.com/why-do-we-write-await-async-in-playwright-javascript-typescript-fa3c92f82841
 });
